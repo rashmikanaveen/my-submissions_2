927a502aaa01f57fd99228b7df215f080cd0a4e7
rashmikanaveen <rashmikanaveen734@gmail.com>
2024-10-01 02:00:46 +0530

    done upto 3.2: Phonebook backend step 2

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 const express = require('express')
 const app = express()
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
+
+
+const Persons  =
+[
+  { 
+    "id": "1",
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
+  { 
+    "id": "2",
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: "32",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  { 
+    "id": "3",
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": "4",
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+
+
+app.get('/api/Persons', (request, response) => {
+  response.json(Persons)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${Persons.length} people<br>${new Date()}</p>`)})
+    
+
 
 const PORT = 3001
 app.listen(PORT, () => {
